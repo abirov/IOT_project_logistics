17ea5cb14a4e4612b1b7fddb0ffc05e76cfa671d
abirov <45330264+abirov@users.noreply.github.com>
2024-06-08 14:55:06 +0200

    Update drivers.py

--- app/models/drivers.py
+++ app/models/drivers.py
@@ -1,1 +1,22 @@
+from pymongo import MongoClient
+from bson import ObjectId
+
+client = MongoClient('localhost', 27017)
+db = client['logistics_db']
+drivers_collection = db['drivers']
+
+def create_driver(data):
+    return drivers_collection.insert_one(data)
+
+def get_driver(driver_id):
+    return drivers_collection.find_one({'_id': ObjectId(driver_id)})
+
+def update_driver(driver_id, data):
+    return drivers_collection.update_one({'_id': ObjectId(driver_id)}, {'$set': data})
+
+def delete_driver(driver_id):
+    return drivers_collection.delete_one({'_id': ObjectId(driver_id)})
+
+def list_drivers():
+    return list(drivers_collection.find())
 
