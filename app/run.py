ad2596da8573e42c5a3c405010574db029dde071
abirov <45330264+abirov@users.noreply.github.com>
2024-06-08 15:04:35 +0200

    Update run.py

--- app/run.py
+++ app/run.py
@@ -7,5 +7,17 @@ def start_web_app():
     subprocess.Popen(["python", "app/web/web_app.py"])
 
 def start_vehicle_simulation():
-    subprocess.Popen(["python", "app/simulations
+    subprocess.Popen(["python", "app/simulations/vehicle_simulation.py"])
 
+def start_telegram_bot():
+    subprocess.Popen(["python", "telegram_bot/bot.py"])
+
+def start_telegraf():
+    subprocess.Popen(["telegraf", "--config", "config/telegraf.conf"])
+
+if __name__ == '__main__':
+    start_catalog_service()
+    start_web_app()
+    start_vehicle_simulation()
+    start_telegram_bot()
+    start_telegraf()
