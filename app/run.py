2bcbed6b0645a665f6d96c7f64b730994608cca9
abirov <45330264+abirov@users.noreply.github.com>
2024-06-08 15:04:04 +0200

    Update run.py

--- app/run.py
+++ app/run.py
@@ -1,1 +1,11 @@
+import subprocess
+
+def start_catalog_service():
+    subprocess.Popen(["python", "app/services/catalog_service.py"])
+
+def start_web_app():
+    subprocess.Popen(["python", "app/web/web_app.py"])
+
+def start_vehicle_simulation():
+    subprocess.Popen(["python", "app/simulations
 
