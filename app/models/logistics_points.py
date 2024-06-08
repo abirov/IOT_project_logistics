b9a7d6c4a1dfb27dfdc9be16dac443615ecfd437
abirov <45330264+abirov@users.noreply.github.com>
2024-06-08 12:38:20 +0200

    Update logistics_points.py

--- app/models/logistics_points.py
+++ app/models/logistics_points.py
@@ -1,1 +1,21 @@
+from pymongo import MongoClient
+
+client = MongoClient('localhost', 27017)
+db = client['logistics_db']
+logistics_points_collection = db['logistics_points']
+
+def create_logistics_point(data):
+    return logistics_points_collection.insert_one(data)
+
+def get_logistics_point(point_id):
+    return logistics_points_collection.find_one({'_id': ObjectId(point_id)})
+
+def update_logistics_point(point_id, data):
+    return logistics_points_collection.update_one({'_id': ObjectId(point_id)}, {'$set': data})
+
+def delete_logistics_point(point_id):
+    return logistics_points_collection.delete_one({'_id': ObjectId(point_id)})
+
+def list_logistics_points():
+    return list(logistics_points_collection.find())
 
