5d90896c1eae905db80f04f20a3debab75c8d89a
abirov <45330264+abirov@users.noreply.github.com>
2024-06-09 11:23:33 +0200

    Update vehicle_simulation.py

--- app/simulations/vehicle_simulation.py
+++ app/simulations/vehicle_simulation.py
@@ -1,22 +1,18 @@
+# /app/simulations/vehicle_simulation.py
 import paho.mqtt.client as mqtt
 import time
 import random
 import json
 
-BROKER = 'localhost'
+BROKER = 'mqtt_broker'
 PORT = 1883
 TOPIC = 'vehicles/status'
 
 def on_connect(client, userdata, flags, rc):
     print(f"Connected with result code {rc}")
-    client.subscribe(TOPIC)
-
-def on_message(client, userdata, msg):
-    print(f"Received message: {msg.payload.decode()}")
 
 client = mqtt.Client()
 client.on_connect = on_connect
-client.on_message = on_message
 client.connect(BROKER, PORT, 60)
 
 def simulate_vehicle_updates(vehicle_id):
@@ -31,4 +27,3 @@ if __name__ == '__main__':
     vehicle_id = '60b85c5f3e8b9a4a988d4e3b'  # Example vehicle ID
     simulate_vehicle_updates(vehicle_id)
     client.loop_forever()
-
