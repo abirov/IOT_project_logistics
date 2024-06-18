fbf4305f29fa3925385f663cf92051f1dc31f40a
abirov <45330264+abirov@users.noreply.github.com>
2024-06-18 21:41:59 +0200

    Create web.py

---
+++ app/web_app/web.py
@@ -0,0 +1,28 @@
+import cherrypy
+from jinja2 import Environment, FileSystemLoader
+import requests
+
+class WebApp:
+    def __init__(self, catalog_url):
+        self.catalog_url = catalog_url
+        self.env = Environment(loader=FileSystemLoader('app/web/templates'))
+
+    @cherrypy.expose
+    def index(self):
+        template = self.env.get_template('index.html')
+        vehicles = self._get_vehicles()
+        logistics_points = self._get_logistics_points()
+        return template.render(vehicles=vehicles, logistics_points=logistics_points)
+
+    def _get_vehicles(self):
+        response = requests.get(f"{self.catalog_url}/vehicles")
+        return response.json()
+
+    def _get_logistics_points(self):
+        response = requests.get(f"{self.catalog_url}/logistics_points")
+        return response.json()
+
+if __name__ == '__main__':
+    catalog_url = 'http://catalog_service:8080'
+    cherrypy.config.update({'server.socket_host': '0.0.0.0', 'server.socket_port': 8081})
+    cherrypy.quickstart(WebApp(catalog_url), '/')
