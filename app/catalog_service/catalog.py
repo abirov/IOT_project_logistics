b8ede53ef64342b50b85cadae54efd334ae0d828
abirov <45330264+abirov@users.noreply.github.com>
2024-10-19 16:15:36 +0200

    Update catalog.py

--- app/catalog_service/catalog.py
+++ app/catalog_service/catalog.py
@@ -1,118 +1,91 @@
+# from models.LogisticsPointRepository import LogisticsPoint
 import cherrypy
-import json
-from bson import ObjectId
-from pymongo import MongoClient
-from models.logistics_point import LogisticsPoint
-from models.vehicle import Vehicle
-from models.driver import Driver
-from models.warehouse import Warehouse
-from models.feedback import Feedback
+from models.vehicleRepository import Vehicle
+from models.driverRepository import Driver
+from models.warehouseRepository import Warehouse
+from models.feedbackRepository import Feedback
 
-class JSONEncoder(json.JSONEncoder):
-    def default(self, o):
-        if isinstance(o, ObjectId):
-            return str(o)
-        return json.JSONEncoder.default(self, o)
 
 class CatalogService:
     def __init__(self):
-        self.client = MongoClient('localhost', 27017)
-        self.db = self.client['logistics_db']
-        self.logistics_point_model = LogisticsPoint(self.db)
-        self.vehicle_model = Vehicle(self.db)
-        self.driver_model = Driver(self.db)
-        self.warehouse_model = Warehouse(self.db)
-        self.feedback_model = Feedback(self.db)
+        self.driver_repo = Driver()
+        self.warehouse_repo = Warehouse()
+        self.feedback_repo = Feedback()
+        self.vehicle_repo = Vehicle()
+        # self.logpoint_repo = LogisticsPoint()
 
-    @cherrypy.expose
-    @cherrypy.tools.json_out()
-    def logistics_points(self, point_id=None):
-        if cherrypy.request.method == 'GET':
-            if point_id:
-                return self.logistics_point_model.get(point_id)
-            return self.logistics_point_model.list()
-        elif cherrypy.request.method == 'POST':
-            data = cherrypy.request.json
-            return self.logistics_point_model.create(data)
-        elif cherrypy.request.method == 'PUT':
-            data = cherrypy.request.json
-            return self.logistics_point_model.update(point_id, data)
-        elif cherrypy.request.method == 'DELETE':
-            return self.logistics_point_model.delete(point_id)
+    ##  DRIVER FUNCTIONALITY
+    def get_driver_by_id(self, driver_id='str'):
+        return self.driver_repo.get_by_id(driver_id)
 
-    @cherrypy.expose
-    @cherrypy.tools.json_out()
-    def vehicles(self, vehicle_id=None):
-        if cherrypy.request.method == 'GET':
-            if vehicle_id:
-                return self.vehicle_model.get(vehicle_id)
-            return self.vehicle_model.list()
-        elif cherrypy.request.method == 'POST':
-            data = cherrypy.request.json
-            return self.vehicle_model.create(data)
-        elif cherrypy.request.method == 'PUT':
-            data = cherrypy.request.json
-            return self.vehicle_model.update(vehicle_id, data)
-        elif cherrypy.request.method == 'DELETE':
-            return self.vehicle_model.delete(vehicle_id)
+    def get_driver_by_name(self, driver_name='str'):
+        return self.driver_repo.get_by_name(driver_name)
 
-    @cherrypy.expose
-    @cherrypy.tools.json_out()
-    def drivers(self, driver_id=None):
-        if cherrypy.request.method == 'GET':
-            if driver_id:
-                return self.driver_model.get(driver_id)
-            return self.driver_model.list()
-        elif cherrypy.request.method == 'POST':
-            data = cherrypy.request.json
-            return self.driver_model.create(data)
-        elif cherrypy.request.method == 'PUT':
-            data = cherrypy.request.json
-            return self.driver_model.update(driver_id, data)
-        elif cherrypy.request.method == 'DELETE':
-            return self.driver_model.delete(driver_id)
+    def put_driver(self, data):
+        return self.driver_repo.create(data)
 
-    @cherrypy.expose
-    @cherrypy.tools.json_out()
-    def warehouses(self, warehouse_id=None):
-        if cherrypy.request.method == 'GET':
-            if warehouse_id:
-                return self.warehouse_model.get(warehouse_id)
-            return self.warehouse_model.list()
-        elif cherrypy.request.method == 'POST':
-            data = cherrypy.request.json
-            return self.warehouse_model.create(data)
-        elif cherrypy.request.method == 'PUT':
-            data = cherrypy.request.json
-            return self.warehouse_model.update(warehouse_id, data)
-        elif cherrypy.request.method == 'DELETE':
-            return self.warehouse_model.delete(warehouse_id)
-    #@cherrypy.expose  # This makes the method accessible via HTTP
-    #@cherrypy.tools.json_out()  # This tells CherryPy to return the output as JSON
-    #def index(self):
-       #  return {"message": "Welcome to the Catalog Service API"}
-    @cherrypy.expose
-    @cherrypy.tools.json_out()
-    @cherrypy.tools.json_in()
-    def feedback(self):
-        if cherrypy.request.method == 'POST':
-            data = cherrypy.request.json
-            driver_id = data.get('driver_id')
-            warehouse_id = data.get('warehouse_id')
-            self.feedback_model.create(data)
-            if driver_id:
-                self.driver_model.update_reputation(driver_id)
-            if warehouse_id:
-                self.warehouse_model.update_reputation(warehouse_id)
-            return {'status': 'success'}
-        elif cherrypy.request.method == 'GET':
-            driver_id = cherrypy.request.params.get('driver_id')
-            warehouse_id = cherrypy.request.params.get('warehouse_id')
-            if driver_id:
-                return self.feedback_model.list_for_driver(driver_id)
-            if warehouse_id:
-                return self.feedback_model.list_for_warehouse(warehouse_id)
+    def update_driver(self, driver_id, data):
+        return self.driver_repo.update(driver_id, data)
 
-if __name__ == '__main__':
-    cherrypy.config.update({'server.socket_host': '0.0.0.0', 'server.socket_port': 8080})
-    cherrypy.quickstart(CatalogService(), '/')
+    def delete_driver(self, driver_id):
+        return self.driver_repo.delete(driver_id)
+
+    def list_all_drivers(self):
+        return self.driver_repo.list_all()
+
+    def update_reputation_driver(self, driver_id, new_average):
+        return self.driver_repo.update_reputation(driver_id, new_average)
+
+    ## Warehouse Functionality
+    def create_warehouse(self, data):
+        return self.warehouse_repo.create(data)
+
+    def get_warehouse_by_id(self, warehouse_id):
+        return self.warehouse_repo.get_by_id(warehouse_id)
+
+    def update_warehouse(self, warehouse_id, data):
+        return self.warehouse_repo.update(warehouse_id, data)
+
+    def delete_warehouse(self, warehouse_id):
+        return self.warehouse_repo.delete(warehouse_id)
+
+    def list_all_warehouses(self):
+        return self.warehouse_repo.list_all()
+
+    def update_reputation_warehouse(self, warehouse_id, new_average):
+        return self.warehouse_repo.update_reputation(warehouse_id, new_average)
+
+    ## Feedback Functionality
+    def create_feedback(self, data):
+        return self.feedback_repo.create(data)
+
+    def get_feedback_by_id(self, f_id):
+        return self.feedback_repo.get_by_object_id(f_id)
+
+    def update_feedback(self, feedback_id, data):
+        return self.feedback_repo.update(feedback_id, data)
+
+    def delete_feedback(self, feedback_id):
+        return self.feedback_repo.delete(feedback_id)
+
+    def get_score(self, f_id):
+        return self.feedback_repo.get_score(f_id)
+
+    ## Vehicle functionality
+    def create_vehicle(self, data):
+        return self.vehicle_repo.create(data)
+
+    def get_vehicle_by_id(self, vehicle_id):
+        return self.vehicle_repo.get_by_id(vehicle_id)
+
+    def update_vehicle(self, vehicle_id, data):
+        return self.vehicle_repo.update(vehicle_id, data)
+
+    def delete_vehicle(self, vehicle_id):
+        return self.vehicle_repo.delete(vehicle_id)
+
+    def list_all_vehicles(self):
+        return self.vehicle_repo.list_all()
+
+
+    expose=True
