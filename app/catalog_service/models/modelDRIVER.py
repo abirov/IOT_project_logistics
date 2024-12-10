e7ef62d43262c3dafe6da504d87b4090e81b1129
Hosseinzn96 <s309247@studenti.polito.it>
2024-12-10 20:38:29 +0100

    Updated model driver

--- app/catalog_service/models/modelDRIVER.py
+++ app/catalog_service/models/modelDRIVER.py
@@ -3,7 +3,7 @@ import uuid
 
 class Driver:
 
-    def __init__(self, name: str, email: str, phone: str, address: str, license_number: str, _id: ObjectId= ObjectId(), vehicle_id:uuid.UUID = uuid.uuid4(), car_model: str):
+    def __init__(self, name: str, email: str, phone: str, address: str, license_number: str, car_model: str, _id: ObjectId = ObjectId(), vehicle_id: uuid.UUID = uuid.uuid4()):
         self.name = name
         self.email = email
         self.phone = phone
@@ -13,10 +13,8 @@ class Driver:
         self.vehicle_id = vehicle_id
         self.car_model = car_model
 
-        
-
     def __str__(self):
-        return f"Driver(id:{self._id} ,name: {self.name}, email: {self.email}, phone: {self.phone}, address: {self.address}, license_number: {self.license_number}, vehicle_id: {self.vehicle_id}, car_model: {self.car_model})"
+        return f"Driver(id: {self._id}, name: {self.name}, email: {self.email}, phone: {self.phone}, address: {self.address}, license_number: {self.license_number}, vehicle_id: {self.vehicle_id}, car_model: {self.car_model})"
     
     def to_dict(self):
         return {
@@ -29,6 +27,7 @@ class Driver:
             "vehicle_id": str(self.vehicle_id),
             "car_model": self.car_model
         }
+
     @staticmethod
     def from_dict(data):
         return Driver(
@@ -37,9 +36,7 @@ class Driver:
             phone=data["phone"],
             address=data["address"],
             license_number=data["license_number"],
-            _id = data.get("_id"),
-            vehicle_id = data.get("vehicle_id"),
-            car_model=data["car_model"]
-
+            car_model=data["car_model"],
+            _id=ObjectId(data.get("_id")),
+            vehicle_id=uuid.UUID(data.get("vehicle_id"))
         )
-    
