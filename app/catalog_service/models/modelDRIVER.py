d4946a925fdf55fd712e94ed60ae273fdd3d3a1b
Hosseinzn96 <134502941+Hosseinzn96@users.noreply.github.com>
2025-05-26 14:38:30 +0200

    Update modelDRIVER.py

--- app/catalog_service/models/modelDRIVER.py
+++ app/catalog_service/models/modelDRIVER.py
@@ -3,14 +3,14 @@ import uuid
 
 class Driver:
 
-    def __init__(self, name: str, email: str, phone: str, address: str, license_number: str, car_model: str, _id: ObjectId = ObjectId(), vehicle_id: uuid.UUID = uuid.uuid4()):
+    def __init__(self, name: str, email: str, phone: str, address: str, license_number: str, car_model: str, _id=None, vehicle_id=None):
+        self._id = _id if _id is not None else ObjectId()
         self.name = name
         self.email = email
         self.phone = phone
         self.address = address
         self.license_number = license_number
-        self._id = _id if _id else ObjectId()
-        self.vehicle_id = vehicle_id
+        self.vehicle_id = vehicle_id if vehicle_id is not None else uuid.uuid4()
         self.car_model = car_model
 
     def __str__(self):
@@ -38,5 +38,5 @@ class Driver:
             license_number=data["license_number"],
             car_model=data["car_model"],
             _id=ObjectId(data.get("_id")),
-             vehicle_id=uuid.UUID(data.get("vehicle_id"))
+            vehicle_id=uuid.UUID(data.get("vehicle_id"))
         )
